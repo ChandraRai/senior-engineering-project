eeb7895e2046901dce599c0486652a84f129b5e7
egoodwinx <emyrocks100@hotmail.com>
2022-01-23 18:43:18 -0500

    missed users.cs

--- Flashminder/Flashminder/Models/FlashminderModel.Designer.cs
+++ Flashminder/Flashminder/Models/FlashminderModel.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'D:\One Drive - ITC Systems\OneDrive - ITC Systems\Desktop\Flashminder\Flashminder\Models\FlashminderModel.edmx'. 
+﻿// T4 code generation is enabled for model 'A:\FinalProject\senior-engineering-project\Flashminder\Flashminder\Models\FlashminderModel.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
--- Flashminder/Flashminder/Models/USERS.cs
+++ Flashminder/Flashminder/Models/USERS.cs
@@ -14,20 +14,25 @@ namespace Flashminder.Models
     
     public partial class USERS
     {
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
+        public USERS()
+        {
+            this.Flashcards = new HashSet<Flashcard>();
+        }
+        public USERS(string username)
+        {
+            this.Username = username;
+            this.Flashcards = new HashSet<Flashcard>();
+        }
+
         public int Id { get; set; }
         public string Username { get; set; }
         public string Password { get; set; }
         public string Email { get; set; }
         public short Privilege { get; set; }
+    
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<Flashcard> Flashcards { get; set; }
 
-
-        public USERS()
-        {
-            
-        }
-        public USERS(string username)
-        {
-            this.Username = username;
-        }
     }
 }
